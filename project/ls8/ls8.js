f25aa72e2b50b58482d1f33c454e167b3be4ae75
Justin Kaseman <justinkaseman@live.com>
2018-05-15 14:55:01 -0700

    Un-hardcode machine code

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,33 +1,33 @@
+const fs = require('fs');
 const RAM = require('./ram');
 const CPU = require('./cpu');
 
+const args = process.argv;
+
+if (args.length != 3) {
+    if (args.length === 2) console.error(' Missing argument: \n\n     provide a file to run')
+    if (args.length > 3) console.error(' Too many arguments: \n\n     can only handle one file')
+    process.exit();
+}
 /**
  * Load an LS8 program into memory
  *
  * TODO: load this from a file on disk instead of having it hardcoded
  */
 function loadMemory() {
-
     // Hardcoded program to print the number 8 on the console
+    try {
+        const regexp = /[0-9]{8}/gi;
+        const program = fs.readFileSync(`${args[2]}`, "utf-8").match(regexp);
 
-    const program = [ // print8.ls8
-        "10011001",
-        "00000000",
-        "00001000",
-        "10011001",
-        "00000001",
-        "00001001",
-        "10101010",
-        "00000000",
-        "00000001",
-        "01000011",
-        "00000000",
-        "00000001"
-    ];
-
-    // Load the program into the CPU's memory a byte at a time
-    for (let i = 0; i < program.length; i++) {
-        cpu.poke(i, parseInt(program[i], 2));
+        //Load the program into the CPU's memory a byte at a time
+        for (let i = 0; i < program.length; i++) {
+            cpu.poke(i, parseInt(program[i], 2));
+        }
+    }
+    catch (err) {
+        console.log('invalid file, try again')
+        process.exit();
     }
 }
 
